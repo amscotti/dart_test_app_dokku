a939339bd8b342e541703ffc5001771d0578551d
Anthony Scotti <anthony.m.scotti@gmail.com>
2013-07-01 21:37:17 -0400

    Fix addString issue

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,7 @@ import 'dart:json' as JSON;
 
 main() {
   var port = int.parse(Platform.environment['PORT']);
-  HttpServer.bind('0.0.0.0', port).then((HttpServer server) {
+  HttpServer.bind('0.0.0.0', port).then((server) {
     print('Server started on port: ${port}');
     server.listen((HttpRequest request) {
       var resp = JSON.stringify({
@@ -12,7 +12,7 @@ main() {
         'Environment': Platform.environment}
       );
       request.response..headers.set(HttpHeaders.CONTENT_TYPE, 'application/json')
-                      ..addString(resp)
+                      ..write(resp)
                       ..close();
     });
   });
